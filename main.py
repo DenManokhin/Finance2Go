1c2f9d7440bebf346efa259808cc9913ae2792c0
Mykhailo <bordun.mykhailo24@gmail.com>
2021-10-26 22:21:49 +0300

    Added first task and logo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # This Python file uses the following encoding: utf-8
+from Finance2Go.task1.SimpleInterest import *
 
 if __name__ == "__main__":
-    pass
+    print(get_interest(5, 100000, 0.25))
